be392ca65c9c5efc0f5a2eda7959e3c510a5a005
Juyoung Whang <huey2269@gamil.com>
2021-03-31 19:58:24 +0900

    Use Propas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+function Food({name, picture }){
+
+  return (
+  <div>
+    <h2>I liek {name}</h2>
+    <img src={picture} alt={name} />
+  </div>
+  );
+}
+
+const foodILike = [
+  {
+    id:1,
+    name: "kimchi",
+    image:
+    "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png"
+  },
+  {
+    id:2,
+    name: "Bibimbap",
+    image:
+    "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/S9Y/image/76pr0EfQi6R21MMIyzefN9hiqIU.jpg"
+  }
+
+];
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name ={dish.name} picture={dish.image} />
+      ))}
     </div>
-  );
+  )
 }
+      
 
 export default App;
